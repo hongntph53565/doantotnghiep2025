791fc3e44163e9fbbbb92adef3fdf59630b9fc8a
hongntph53565 <hongnthiph53565@gmail.com>
2025-06-07 23:16:44 +0700

    hhe

--- database/migrations/2025_06_03_124818_create_movies_table.php
+++ database/migrations/2025_06_03_124818_create_movies_table.php
@@ -21,23 +21,14 @@ return new class extends Migration
             $table->text('cast')->nullable();
             $table->date('release_date');
             $table->date('end_date')->nullable();
-
-<<<<<<< HEAD
-            $table->string('poster',255)->nullable();
-            $table->string('trailer',255)->nullable();
-            $table->string('age_rating', 10);
-=======
             $table->string('poster', 255)->nullable();
             $table->string('trailer', 255)->nullable();
             $table->enum('age_rating', ['P', 'T13', 'T18']);
->>>>>>> hong
             $table->enum('format', ['2D', '3D', 'IMAX'])->nullable();
             $table->enum('language', ['Tiếng Việt', 'Tiếng Anh', 'Tiếng Hàn', 'Tiếng Nhật']);
             $table->text('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
-
-            // Foreign Key
             $table->foreign('genre_id')->references('genre_id')->on('genres')->onDelete('cascade');
         });
     }
