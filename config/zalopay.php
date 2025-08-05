1e6154616df17be9339094504048f50495562c87
vũ văn hoàng <vuvanhoang@MacBook-Air-cua-vu.local>
2025-08-05 22:56:31 +0700

    chan

--- config/zalopay.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    "app_id" => 2553,
-    "key1" => "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL",
-    "key2" => "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
-    "endpoint" => "https://sb-openapi.zalopay.vn/v2/create"
-];
